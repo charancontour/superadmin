eb3f5bbe23f6681efd4d5b84f6af0689e7aa915a
charan <charan@contourgroupinc.com>
2016-11-22 13:05:10 +0530

    added fileds to the add to app

--- src/SuperAdminController.php
+++ src/SuperAdminController.php
@@ -427,29 +427,49 @@ class SuperAdminController extends Controller {
 
   public function adduserfromefront(Request $request)
   {
+    $this->validate($request,[
+      'login'=>'required',
+      'firstname'=> 'required',
+      'lastname'=> 'required',
+      'email'=>'required',
+      'password'=>'required',
+      'efront_user_id'=>'required|numeric|min:1',
+      'efront_branch_id'=>'required|numeric|min:1'
+    ]);
     $input = $request->all();
-    dd($input);
+    $branch = branch::where('efront_branch_id',$input['efront_branch_id'])->where('efront_branch_id','!=',0)->first();
+    if(!$branch){
+      return response()->json(['error'=>'Efront branch doesnot exists with our Database.']);
+    }
+    $location = Location::where('location_name',$branch->efront_branch_name)->first();
+    if(!$location){
+      return response()->json(['error'=>'Location doesnot exists with our Database.']);
+    }
     $strposition = strpos($input['login'],Config::get('efront.LoginPrefix'));
     if($strposition != 0 && $strposition === false){
       $input['login'] = Config::get('efront.LoginPrefix').$input['login'];
     }
     $validate_user = User::where('login',$input['login'])
-                          ->orWhere('email',$input['email'])
                           ->first();
     if(!$validate_user){
-      $result = json_decode($this->efront->EditUser($input->id,$input));
+      $result = json_decode($this->efront->EditUser($input['efront_user_id'],$input));
       if($result->success){
         $user = User::create(['login'=>$input['login'],
                       'firstname'=>$input['firstname'],
                       'lastname'=>$input['lastname'],
                       'email'=>$input['email'],
                       'password'=> Hash::make($input['password']),
+                      'location_id'=>$location_id,
+                      'branch_id'=>$branch_id,
+                      'role_id'=>$this->student_role_id,
+                      'efront_user_id'=>$input['efront_user_id'],
+                      'user_status_id'=>$this->active_status_id,
                 ]);
       }else{
-        return response()->json(['error'=>'Couldnt be edited in efront.']);
+        return response()->json(['error'=>'Couldnot be edited in efront.']);
       }
     }else{
-      return response()->json(['error'=>'Login or Email is already taken.']);
+      return response()->json(['error'=>'Login is already taken.']);
     }
     return response()->json(['success'=>'User created Successfully with prefix.']);
   }
